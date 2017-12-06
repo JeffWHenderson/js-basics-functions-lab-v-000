ea38b57b3d457498a1d4b21018f960b8790e19ce
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-06 12:19:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Code your solution in this file!
- 264 feet long
-
- function calculateVertical(start, finish) {
+function calculateVertical(start, finish) {
    return (finish - start) * 264
- }
+}
