05adbfa7b1836395c191cd27bec59ab942e29b78
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-06 12:41:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function calculatesFarePrice(start, end) {
     case distance < 400:
       return 0
     default:
-      return  distance//'cannot travel that far'
+      return  distance - 400//'cannot travel that far'
   }
   // first 400 feet are free
     // return 0
